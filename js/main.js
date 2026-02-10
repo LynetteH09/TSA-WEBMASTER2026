142776a1589fb1abeeefd8f4f1c4439528027649
Your Name <you@example.com>
2026-02-10 07:22:15 -0600

    main page (mod later)

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// current placeholder, will later link toa detailed page about community
+document.getElementById("learnMoreBtn").addEventListener("click", () => {
+  alert("This will later link to a detailed page about the community.");
+});
